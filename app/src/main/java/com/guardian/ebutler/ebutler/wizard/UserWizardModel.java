2182735c8021cf570cb5a6857921aef9dd4cee61
Thanh Hoang Nguyen <nthoang@apcs.vn>
2016-01-25 14:34:43 +0700

    Add Romanurik Android-WizardPager library

---
+++ app/src/main/java/com/guardian/ebutler/ebutler/wizard/UserWizardModel.java
@@ -0,0 +1,19 @@
+package com.guardian.ebutler.ebutler.wizard;
+
+import com.example.android.wizardpager.wizard.model.*;
+import android.content.Context;
+
+public class UserWizardModel extends AbstractWizardModel {
+    public UserWizardModel(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected PageList onNewRootPageList() {
+        throw new UnsupportedOperationException();
+//        return new PageList(
+//            new
+//        );
+
+    }
+}
