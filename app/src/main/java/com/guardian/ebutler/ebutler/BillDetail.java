cf7f3ebed05ed6f2de0c1b5804118f11ca86e165
Anh Phan <phanh@apcs.vn>
2016-01-26 11:04:06 +0700

    Add bill, category draft

---
+++ app/src/main/java/com/guardian/ebutler/ebutler/BillDetail.java
@@ -0,0 +1,13 @@
+package com.guardian.ebutler.ebutler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BillDetail extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_detail);
+    }
+}
