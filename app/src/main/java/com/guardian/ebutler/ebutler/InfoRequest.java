7c1d2f4321c0aeba91b00412698fe0749af4f1b8
tab <nguyenkhactunganh@gmail.com>
2016-03-13 23:49:59 +0700

    add textboxFragment

--- app/src/main/java/com/guardian/ebutler/ebutler/InfoRequest.java
+++ app/src/main/java/com/guardian/ebutler/ebutler/InfoRequest.java
@@ -1,13 +1,16 @@
 package com.guardian.ebutler.ebutler;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.guardian.ebutler.ebutler.wizard.UserWizard;
+import com.guardian.ebutler.fragments.TextboxFragment;
 
 public class InfoRequest extends Activity {
 
